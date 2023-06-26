2959086b4b1974f1c8d5d7b2b9ae371a6ee6cce9
mcramileux <kristineramilo21@gmail.com>
2023-06-26 17:08:15 +1000

    updated folders

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,3 +1,5 @@
+//Followed the solved folder in the mini-project
+
 const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth');
