3971cfe71b824ca1545fb34522ac8c8607336e76
mcramileux <kristineramilo21@gmail.com>
2023-07-08 22:36:31 +1000

    fixing css

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,9 @@
 // followed the mini-project's solved folder
 
-// Format date as MM/DD/YYYY
+// Format date as DD/MM/YYYY
 module.exports = {
   format_date: (date) => {
-      return date.toLocaleDateString();
+      // return date.toLocaleDateString();
+      return `${new Date(date).getDate()}/${new Date(date).getMonth() + 1}/${new Date(date).getFullYear()}`;  
   },
 };
